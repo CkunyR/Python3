33e8bd13f7217fa138847602d9b7b79a32e0e1a0
Zhenkun Guo <1608229093@qq.com>
2022-03-16 23:01:33 +0800

    入门

---
+++ py3.py
@@ -0,0 +1,21 @@
+# 输入函数
+present=input('123456')
+print(present)
+
+# 计算两个整数和
+a=int(input('整数一:'))
+b=int(input('整数二:'))
+#a=int(a)
+#b=int(b)
+print('结果：',a+b)
+
+
+print(11//2) #整除
+print(11%2) # 1 取余运算
+print(2**2) #2的2次方
+print(9//-4) #
+print(-9//4) #一正一负向下取整
+# 取余（一正一负）遵循公式
+# 余数=被除数-除数*商
+print(-9%4) #3
+print(9%-4) #-3
